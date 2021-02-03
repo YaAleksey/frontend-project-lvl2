812589a8273ddb623628d20810b74a906b3aa52b
diemen <a.hikolaev89@gmail.com>
2021-02-03 16:14:56 +0500

    logic for step3

--- bin/genDiff.js
+++ bin/genDiff.js
@@ -1,11 +1,6 @@
 #! /usr/bin/env node
-
-import fs from 'fs';
 import pkg from 'commander';
-import path from 'path';
-import _ from 'lodash';
-
-//console.log(fs);
+import { byExport } from '../src/index.js';
 
 const { program } = pkg;
 
@@ -15,39 +10,6 @@ program
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>');
 
-//  const test = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), "testFile.json"), "utf-8"));
-
-  const textFileOne = JSON.parse(fs.readFileSync('/home/aleksey/frontend-project-lvl2/src/file1.json'), "utf-8");
-  const keysValuesFileOne = Object.entries(textFileOne);
-
-  const textFileTwo = JSON.parse(fs.readFileSync('/home/aleksey/frontend-project-lvl2/src/file2.json'), "utf-8");
-  const keysValuesFileTwo = Object.entries(textFileTwo);
-
-  const result = {};
-
-  const compairKeysFromFileOne = (keysValuesFileOne) => keysValuesFileOne.map(([keyFileOne, valueFileOne]) => {
-//    const resultCompairFunction = {};
-    if (!(_.has(textFileTwo, keyFileOne))) {
-      return result[`- ${keyFileOne}`] = valueFileOne;
-    }
-    if (textFileTwo[keyFileOne] === valueFileOne) {
-      return result[keyFileOne] === valueFileOne;
-    }
-    result[`- ${keyFileOne}`] = valueFileOne;
-    result[`+ ${keyFileOne}`] = textFileTwo[keyFileOne];
-    return result;
-  });
-
-  const compairKeysFromFileTwo = (keysValuesFileTwo) => keysValuesFileTwo.map(([keyFileTwo, valueFileTwo]) => {
-    const objectForCompair = compairKeysFromFileOne(keysValuesFileOne);
-//    console.log(objectForCompair);
-    if (!(_.has(objectForCompair, keyFileTwo))) {
-      return result[`+ ${keyFileTwo}`] = valueFileTwo;
-    }
-  });
-
-  compairKeysFromFileTwo(keysValuesFileTwo);
-  console.log(Object.entries(result).sort());
 
 //  .command('start <arg>')
 
@@ -57,3 +19,4 @@ program
 
 program.parse(process.argv);
 
+console.log(byExport());
--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
+import _ from 'lodash';
 import fs from 'fs';
 
+ const textFileOne = JSON.parse(fs.readFileSync('/home/aleksey/frontend-project-lvl2/src/file1.json'), "utf-8");
+  const keysValuesFileOne = Object.entries(textFileOne);
+
+  const textFileTwo = JSON.parse(fs.readFileSync('/home/aleksey/frontend-project-lvl2/src/file2.json'), "utf-8");
+  const keysValuesFileTwo = Object.entries(textFileTwo);
+
+const iteratingFirstToSecond = (arrayKeysValueFirst, objSecondFile) => {
+  const afterIterating = {};
+  arrayKeysValueFirst.map(([key, value]) => {
+    if (!(_.has(objSecondFile, key))) {
+      return afterIterating[`- ${key}`] = value;
+    }
+    if (objSecondFile[key] === value) {
+      return afterIterating[key] = value;
+    }
+    if (objSecondFile[key] !== value) {
+      afterIterating[`- ${key}`] = value;
+      afterIterating[`+ ${key}`] = objSecondFile[key];
+      return afterIterating;
+    }
+  })
+  return afterIterating;
+};
+
+export const iteratingSecondToFirst = (arrayKeysValueFirst, arrayKeysValueSecond, objFirstFile, objSecondFile) => {
+  const finalResult = iteratingFirstToSecond(arrayKeysValueFirst, objSecondFile);
+  arrayKeysValueSecond.map(([key, value]) => {
+    if (!(_.has(objFirstFile, key))) {
+      return finalResult[`+ ${key}`] = value; 
+    }
+  })
+//  console.log(finalResult);
+  return finalResult;
+};
+
+export const byExport = () => {
+ return iteratingSecondToFirst(keysValuesFileOne, keysValuesFileTwo, textFileOne, textFileTwo);
+};
 
