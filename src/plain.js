329bc6a58c989b6791efe8e022f8b8fd7f1f5a6a
diemen <a.hikolaev89@gmail.com>
2021-05-31 23:11:56 +0500

    create trial plain version 1

--- src/plain.js
+++ src/plain.js
@@ -1,43 +1,68 @@
 import _ from 'lodash';
 
 const connectionFilesInOutput = (tree) => {
-  const lines = tree.map((currentObj) => {
-    if (!(_.has(currentObj, 'children'))) {
-      if (currentObj.status === 'deleted') {
-        return `Property '${currentObj.key}' was removed`;
-      }
-      if (currentObj.status === 'unchanged') {
-        return '';
-      }
-      if (typeof currentObj.value === 'object' && !Array.isArray(currentObj.key)) {
-       return `Property '${currentObj.key}' was added with value: [complex value]`;
-      }
-      return  `Property '${currentObj.key}' was added with value: ${currentObj.value}`;
+
+  const checkForObj = (valueForCheck) => {
+    if (typeof valueForCheck === 'object' && !Array.isArray(valueForCheck) && valueForCheck !== null) {
+      return `[complex value]`;
     }
-  const wayForIter = [];
+    return `${valueForCheck}`;
+  };
 
-  const iter (objWithChld) => {
-    wayForIter.push(objWithChld.key);
+  const output = (tree) => {
+    return tree.map((currentLevel) => {
 
-    const result = objWithChld.children.reduce((acc, child) => {
-      if (child.status === 'unchanged') {
-        acc.push('');
-        return acc;
-      }
+    const iter = (children, wayForIter) => {
+      const result = children.map((child) => {
+
+        if (child.status === 'unchanged') {
+          return '';
+        }
+
+        if (!(_.has(child, 'children'))) {
+
+          if (child.status === 'deleted') {
+            return `Property '${wayForIter.join('.')}' was removed`;
+          }
+
+          if (child.status === 'added') {
+            return `Property '${wayForIter.join('.')}' was added with value: ${checkForObj(child.value)}`;
+          }
+
+          return `Property '${wayForIter.join('.')}' was update. From ${checkForObj(child.oldValue)}
+          to ${checkForObj(child.newValue)}`;
+        }
+
+        wayForIter.push(child.key)
+        return iter(child.children, wayForIter);
+      });
+      return result;
+    };
+
+      if (currentLevel.status === 'unchanged') {
+        return '';
+      };
+
+      if (!(_.has(currentLevel, 'children'))) {
+        if (currentLevel.status === 'deleted') {
+          return `Property '${currentLevel.key}' was removed`;
+        }
+
+        if (currentLevel.status === 'added') {
+          return `Property '${currentLevel.key}' was added with value: ${checkForObj(currentLevel.value)}`;
+        }
+
+        if (currentLevel.status === 'modified') {
+          return `Property '${currentLevel.key}' was update. From ${checkForObj(currentLevel.oldValue)}
+        to ${checkForObj(currentLevel.newValue)}`;
+        }
+      };
       
-      if (!(_.has(child, 'children'))) {
-        if (child.status === '') {
-	  
-	}
-      }
-    }, []);
-  }
-  
-  
-  
-  });
-  
-  return lines;
+      return iter (currentLevel.children, [currentLevel.key]);
+    });
+  };
+
+  return output(tree);
 };
 
 export default connectionFilesInOutput;
