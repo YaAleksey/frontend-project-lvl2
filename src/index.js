cda5daafc946a6c58d51be5c0cfaa4dc8b8acd44
diemen <a.hikolaev89@gmail.com>
2021-10-26 11:38:13 +0500

    improvement maintainbility

--- src/index.js
+++ src/index.js
@@ -3,32 +3,34 @@ import objectOrNot from './objectOrNot.js';
 import parsingFile from './chooseParser.js';
 import format from './formatters/formattersList.js';
 
-
 const genTree = (obj1, obj2) => {
   const uniqueKeys = _.sortBy(_.uniq([...Object.keys(obj1), ...Object.keys(obj2)]));
 
   const counter = uniqueKeys.reduce((acc, key) => {
-    if (obj1[key] === obj2[key]) {
-      acc.push({ key, value: obj1[key], status: 'unchanged' });
-      return acc;
+    if (_.has(obj1, key) && _.has(obj2, key)) {
+      if (obj1[key] === obj2[key]) {
+        acc.push({ key, value: obj1[key], status: 'unchanged' });
+        return acc;
+      }
+      if (!(objectOrNot(obj1[key]) && (objectOrNot(obj2[key])))) {
+        acc.push({
+          key, oldValue: obj1[key], status: 'modified', newValue: obj2[key],
+        });
+        return acc;
+      }
+      acc.push({
+        key, value: 'nested', status: 'changed', children: (genTree(obj1[key], obj2[key])),
+      });
+      return _.sortBy(acc, ['key']);
     }
 
-    if (_.has(obj1, key) && (!(_.has(obj2, key)))) {
+    if (_.has(obj1, key)) {
       acc.push({ key, value: obj1[key], status: 'deleted' });
       return acc;
     }
 
-    if (_.has(obj2, key) && (!(_.has(obj1, key)))) {
-      acc.push({ key, value: obj2[key], status: 'added' });
-      return acc;
-    }
-
-    if (!(objectOrNot(obj1[key]) && (objectOrNot(obj2[key])))) {
-      acc.push({key, oldValue: obj1[key], status: 'modified', newValue: obj2[key]});
-      return acc;
-    }
-    acc.push({key, value: 'nested', status: 'changed', children: (genTree(obj1[key], obj2[key]))});
-    return _.sortBy(acc, ['key']);
+    acc.push({ key, value: obj2[key], status: 'added' });
+    return acc;
   }, []);
 
   return counter;
--- src/objectOrNot.js
+++ src/objectOrNot.js
@@ -2,5 +2,5 @@ export default (val) => {
   if (val === null) {
     return false;
   }
-  return (typeof val  === 'object' && !Array.isArray(val));
+  return (typeof val === 'object' && !Array.isArray(val));
 };
