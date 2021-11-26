e24e2447702e2ea56f86a5214a2963b3d41188eb
diemen <a.hikolaev89@gmail.com>
2021-11-26 15:48:36 +0500

    excluding readfile from chooseParser.js

--- src/chooseParser.js
+++ src/chooseParser.js
@@ -1,15 +1,14 @@
 import path from 'path';
 import yaml from 'js-yaml';
-import readFile from './readFile.js';
 
-const parsingFile = (fileName) => {
+const parsingFile = (fileName, contentFile) => {
   const format = path.extname(fileName);
 
-  if (format === '.yml') {
-    return yaml.load(readFile(fileName));
+  if (format === '.yml' || format === '.yaml') {
+    return yaml.load(contentFile);
   }
 
-  return JSON.parse(readFile(fileName));
+  return JSON.parse(contentFile);
 };
 
 export default parsingFile;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import genTree from './genTree.js';
 import parsingFile from './chooseParser.js';
 import formats from './formatters/formattersList.js';
+import readFile from './readFile.js';
 
 const genDiff = (file1, file2, formatName = 'stylish') => {
-  const firstObj = parsingFile(file1);
-  const secondObj = parsingFile(file2);
+  const readFirst = readFile(file1);
+  const readSecond = readFile(file2);
+
+  const firstObj = parsingFile(file1, readFirst);
+  const secondObj = parsingFile(file2, readSecond);
+
   const diff = genTree(firstObj, secondObj);
   const chooseFormat = formats(formatName);
 
